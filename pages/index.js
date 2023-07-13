f2a3eedf29c27fdfc0637c4930c78bed9f291da8
Jose Gavilanes <j.o.gavilanestello@student.utwente.nl>
2023-07-13 12:54:25 +0200

    supabase added

--- pages/index.js
+++ pages/index.js
@@ -5,149 +5,70 @@ import Script from 'next/script';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Card from '../components/card.component';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://sjdfbnxfumfzrtnlovns.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqZGZibnhmdW1menJ0bmxvdm5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg4NTM1NDEsImV4cCI6MjAwNDQyOTU0MX0.ID_R00L0lGWWcmW0zxtp0u3Ww46364W9MeJXVNoNIhE'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getPeople() {
+  let { data: Runner, error } = await supabase
+    .from('User')
+    .select('*')
+  return Runner
+}
+
+
+
+export async function getStaticProps() {
+  const data = await getPeople()
+  const allPostsData = data
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+
+
 
 function Header({ title }) {
   return <>
-    <Script
+    {/* <Script
       src="https://connect.facebook.net/en_US/sdk.js"
       strategy="lazyOnload"
       onLoad={() => {
         console.log('FB SDK loaded');
       }
       }
-    />
+    /> */}
     <Head>
       <title>{siteTitle}</title>
     </Head>
     <section className={utilStyles.headingMd}>
-      <p>I amm Joe and I am trying to learn Next.js ;)</p>
+      <p>I am Joe and I am trying to learn Next.js ;)</p>
     </section>
     <h1>{title ? title : 'Default title'}</h1>
   </>;
 }
 
-function HomePage() {
-
-  const people = [
-    {
-      name: 'Write thesis',
-      link: '',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Research papers',
-      link: '/learning/research-papers',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Hobbies',
-      link: '',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Learn Next.js',
-      link: '/learning/nextjs',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Learn React',
-      link: '/learning/react',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Learn Supabase',
-      link: '/learning/supabase',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Learn ChatGPT API',
-      link: '/learning/chatgpt',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Learn Django',
-      link: '/learning/django',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Learn Supabase',
-      link: '/learning/supabase',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'My DataBases',
-      link: '/learning/databases',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-  ];
+export default function HomePage({ allPostsData }) {
 
   const [likes, setLikes] = useState(0);
-
-
 
   function handleClick() {
     setLikes(likes + 1);
   }
   return (
     <Layout home>
-
       <Head><title>First Thingy</title></Head>
       <Header title="My way to sky🚀" />
-      <Card people={people} />
+      <Card people={allPostsData} />
       <button type='button' className="rounded-full px-6 py-3 bg-gradient-to-r from-gray-400 to-blue-500 hover:from-black-500 hover:to-gray-500" onClick={handleClick}>Like ({likes})</button>
 
+      <dic>{console.log(allPostsData)}</dic>
     </Layout>
   );
 }
-
-export default HomePage;
