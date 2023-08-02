b6724af624d05865de5eb623f95ed95bb9b76302
Jose Gavilanes <j.o.gavilanestello@student.utwente.nl>
2023-08-02 19:08:19 +0200

    commented SERVERSIDE

--- pages/index.js
+++ pages/index.js
@@ -39,13 +39,13 @@ export async function getStaticProps() {
   };
 }
 
-export async function getServersideProps(context) {
-  return {
-    props: {
+// export async function getServersideProps(context) {
+//   return {
+//     props: {
 
-    }
-  }
-}
+//     }
+//   }
+// }
 
 
 
