8c4753f2d4ba1787b57668a2ae55b2ff50e9a0f3
Jose Gavilanes <j.o.gavilanestello@student.utwente.nl>
2023-08-03 21:41:25 +0200

    dark drawer

--- components/dashboard/sidebar.component.js
+++ components/dashboard/sidebar.component.js
@@ -19,13 +19,13 @@ function Sidebar({ children }) {
             />
             <label
                 htmlFor="drawer-toggle"
-                className={`fixed initial top-0 left-0 inline-block p-3 transition-all duration-500 bg-blue-300 z-40 rounded-lg ${drawerOpen ? 'rotate-180  pt-5 pb-5 ml-[1.19px]' : ''}`}
+                className={`fixed initial top-0 left-0 inline-block p-3 transition-all duration-500 bg-black z-40 rounded-lg ${drawerOpen ? 'rotate-180  pt-5 pb-5 ml-[1.19px]' : ''}`}
             >
                 <div className={`h-1 -rotate-45 bg-white rounded-lg ${drawerOpen ? 'w-[35px] -mb-[4px] ' : 'w-4 mb-1.5 '}`}></div>
                 <div className={`h-1 rotate-45 bg-white rounded-lg ${drawerOpen ? 'w-[35px]' : 'w-4 '}`}></div>
             </label>
 
-            <div className='fixed top-0 left-0 z-20  h-full transition-all duration-500 transform -translate-x-full bg-white shadow-lg peer-checked:translate-x-0 '>
+            <div className='fixed w-15 top-0 left-0 z-20  h-full transition-all duration-500 transform -translate-x-full bg-black shadow-lg peer-checked:translate-x-0 '>
                 <div className='flex flex-col space-y-4 '>
                     <Link href='/'>
                         <div className='bg-blue-100 rounded-lg inline-block text-black'>
@@ -33,34 +33,34 @@ function Sidebar({ children }) {
                             {/* <AiOutlineDashboard size={50} /> */}
                         </div>
                     </Link>
-                    <span className="border-b-[1px] border-gray-200 w-full "></span>
+                    <span className="border-b-[1px] border-gray-200 w-full p-1"></span>
 
                     <div className='grid grid-cols-1 items-end space-y-5'>
                         <Link className='pt-5' href='#'>
                             <div className='cursor-pointer flex flex-col items-center text-black'>
-                                <FaNetworkWired className='hover:bg-gray-50 rounded-lg ' color='#d1d5db' size={50} />
-                                <span className="text-white text-sm font-medium" >Projects</span>
+                                <FaNetworkWired className='hover:bg-gray-50 rounded-lg ' color='#d1d5db' size={40} />
+                                <span className="text-white text-xs font-medium" >Projects</span>
                             </div>
                         </Link>
 
                         <Link href='posts/dogs'>
                             <div className=' cursor-pointer flex flex-col justify-center items-center text-black'>
-                                <BsFillFileEarmarkPostFill className='hover:bg-gray-50 rounded-lg ' color='#d1d5db' size={50} />
-                                <span className="text-white text-sm font-medium">Posts</span>
+                                <BsFillFileEarmarkPostFill className='hover:bg-gray-50 rounded-lg ' color='#d1d5db' size={40} />
+                                <span className="text-white text-xs font-medium">Posts</span>
                             </div>
                         </Link>
 
                         <Link href='#'>
                             <div className=' cursor-pointer  flex flex-col justify-center items-center text-black'>
-                                <BiTask className='hover:bg-gray-50 rounded-lg ' color='#d1d5db' size={50} />
-                                <span className="text-sm font-medium text-white">Manager</span>
+                                <BiTask className='hover:bg-gray-50 rounded-lg ' color='#d1d5db' size={40} />
+                                <span className="text-xs p-1 font-medium text-white">Manager</span>
                             </div>
                         </Link>
                     </div>
                 </div>
             </div>
 
-            <main className=' w-full bg-gray-100 min-h-screen '>{children}</main>
+            <main className=' w-full bg-white min-h-screen '>{children}</main>
         </div>
 
     );
