a5e521aef88c93eaf7d642dfd9932a95b1bae9d3
Jose Gavilanes <j.o.gavilanestello@student.utwente.nl>
2023-08-03 21:14:59 +0200

    drawer even improved

--- components/dashboard/sidebar.component.js
+++ components/dashboard/sidebar.component.js
@@ -19,10 +19,10 @@ function Sidebar({ children }) {
             />
             <label
                 htmlFor="drawer-toggle"
-                className={`fixed initial top-0 left-0 inline-block p-3 transition-all duration-500 bg-blue-300 z-40 rounded-lg ${drawerOpen ? 'rotate-180  p-5 pl-5 pr-5' : ''}`}
+                className={`fixed initial top-0 left-0 inline-block p-3 transition-all duration-500 bg-blue-300 z-40 rounded-lg ${drawerOpen ? 'rotate-180  pt-5 pb-5 ml-[1.19px]' : ''}`}
             >
-                <div className="w-4 h-1 mb-1.5 -rotate-45 bg-white rounded-lg"></div>
-                <div className="w-4 h-1 rotate-45 bg-white rounded-lg"></div>
+                <div className={`h-1 -rotate-45 bg-white rounded-lg ${drawerOpen ? 'w-[35px] -mb-[4px] ' : 'w-4 mb-1.5 '}`}></div>
+                <div className={`h-1 rotate-45 bg-white rounded-lg ${drawerOpen ? 'w-[35px]' : 'w-4 '}`}></div>
             </label>
 
             <div className='fixed top-0 left-0 z-20  h-full transition-all duration-500 transform -translate-x-full bg-white shadow-lg peer-checked:translate-x-0 '>
